d444ca6bd9b3542fe46146d9cfed5716ae2cddf6
Daisy <92063434+Anderanged@users.noreply.github.com>
2024-03-16 15:15:21 -0600

    readable macros for alterPool

--- addons/base/defines.hpp
+++ addons/base/defines.hpp
@@ -2,3 +2,7 @@
 #include "macro_defines.hpp"
 
 #define RPFLIM_MAX 16384 // 2^14
+#define ADD_CLAMP [false,false]
+#define SUB_CLAMP [true,false]
+#define ADD_REJCT [false,true]
+#define SUB_REJCT [true,true]
